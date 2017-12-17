f9f05e065c24a3b64db2af8199b4fbcd0c11a9e3
Warut Surapat <s.warut@gmail.com>
2017-12-18 00:56:50 +0900

    Update config.

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :csalunet, Csalunet.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "swarut",
+  password: "",
   database: "csalunet_dev",
   hostname: "localhost",
   pool_size: 10
