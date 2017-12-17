2d5c3f1a549c71a848533a17c00d7e2596fa33e8
Warut Surapat <s.warut@gmail.com>
2017-12-18 00:57:13 +0900

    Add Facebook deps.

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Csalunet.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:facebook, "~> 0.17.0"}
     ]
   end
 
